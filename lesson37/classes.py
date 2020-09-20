10013a9489e433842165c00d7a292de45382b521
Valerii Voronkov <scorpirat@gmail.com>
2020-09-20 13:00:33 +0300

    Add lesson37 classes with constructors.

---
+++ lesson37/classes.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name): #constructor
+        print('Hi')
+
+    def print_info(self):
+        print(f'whatever {self.name}')
---
+++ lesson37/lesson37.py
@@ -0,0 +1,9 @@
+#Classes with constructor
+
+from classes import Person
+
+person1 = Person()
+person1.print_info()
+
+person2 = Person()
+person2.print_info()
